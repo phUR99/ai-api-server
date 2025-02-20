e739c1efd29ca2d35c6ed187e6c71b2f23727546
phUR99 <122969226+phUR99@users.noreply.github.com>
2025-02-20 14:07:39 +0900

    add XorModel, OrModel

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from typing import Union
 import model
 from fastapi import FastAPI
 
-model = model.AndModel()
+modelAnd = model.AndModel()
+modelXor = model.XorModel()
+modelOr = model.OrModel()
 app = FastAPI()
 
 @app.get("/")
@@ -13,12 +15,30 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/predict/left/{left}/right/{right}") #endpoint
+@app.get("/predictAnd/left/{left}/right/{right}") #endpoint
 def predict(left: int, right: int):
-    result = model.predict([left, right])
+    result = modelAnd.predict([left, right])
     return {"result": result}
 
-@app.post("/train")
+@app.get("/predictOr/left/{left}/right/{right}") #endpoint
+def predict(left: int, right: int):
+    result = modelOr.predict([left, right])
+    return {"result": result}
+
+@app.get("/predictXor/left/{left}/right/{right}") #endpoint
+def predict(left: int, right: int):
+    result = modelXor.predict([left, right])
+    return {"result": result}
+
+@app.post("/trainAnd")
 def train():
-    model.train()
+    modelAnd.train()    
     return {"result" : "OK"}
+@app.post("/trainOr")
+def train():
+    modelOr.train()
+    return {"result" : "OK"}
+@app.post("/trainXor")
+def train():
+    modelXor.train()
+    return {"result" : "OK"}    
--- model.py
+++ model.py
@@ -1,4 +1,8 @@
 import numpy as np
+import torch
+import torch.nn as nn
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 
 class AndModel:
     def __init__(self):
@@ -35,4 +39,77 @@ class AndModel:
     def predict(self, input_data):
         total_input = np.dot(input_data, self.weights) + self.bias
         return self.step_function(total_input)    
+
+class XorModel(nn.Module):
+    def __init__(self):
+        super(XorModel, self).__init__()
+        self.layer1 = nn.Linear(2, 10, bias=True)
+        self.layer2 = nn.Linear(10, 10, bias=True)
+        self.layer3 = nn.Linear(10, 10, bias=True)
+        self.layer4 = nn.Linear(10, 1, bias=True)
+        self.relu = nn.ReLU()
+        self.to(device)
+        
+    def forward(self, x):
+        x = self.relu(self.layer1(x))
+        x = self.relu(self.layer2(x))   
+        x = self.relu(self.layer3(x))
+        x = self.layer4(x)
+        return x
+        
+    def train(self):
+        learning_rate = 0.1
+        epochs = 100
+        inputs = torch.FloatTensor([[0, 0], [0, 1], [1, 0], [1, 1]]).to(device)
+        outputs = torch.FloatTensor([[0], [1], [1], [0]]).to(device)
+        criterion = nn.BCEWithLogitsLoss()
+        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate, momentum=0.9)
+        for epoch in range(epochs):
+            optimizer.zero_grad()
+            output = self(inputs)
+            loss = criterion(output, outputs)
+            loss.backward()
+            optimizer.step()
+            print(f'epoch: {epoch}, loss: {loss.item()}')
+
+    def predict(self, input_data):
+        input_data = torch.FloatTensor(input_data).to(device)
+        output = torch.sigmoid(self(input_data)).item()
+        return output > 0.5  
+    
+class OrModel(nn.Module):
+    def __init__(self):
+        # 파라메터
+        super(OrModel, self).__init__()
+        self.layer1 = nn.Linear(2, 10, bias=True)
+        self.layer2 = nn.Linear(10, 10, bias=True)
+        self.layer3 = nn.Linear(10, 1, bias=True)
+        self.relu = nn.ReLU()
+        self.to(device)
+    
+    def forward(self, x):
+        x = self.relu(self.layer1(x))
+        x = self.relu(self.layer2(x))   
+        x = self.layer3(x)
+        return x
+    
+    def train(self, mode = True):
+        learning_rate = 0.1
+        epochs = 100
+        inputs = torch.FloatTensor([[0, 0], [0, 1], [1, 0], [1, 1]]).to(device)
+        outputs = torch.FloatTensor([[0], [1], [1], [1]]).to(device)
+        criterion = nn.BCEWithLogitsLoss()
+        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate, momentum=0.9)
+        for epoch in range(epochs):
+            optimizer.zero_grad()
+            output = self(inputs)
+            loss = criterion(output, outputs)
+            loss.backward()
+            optimizer.step()
+            print(f'epoch: {epoch}, loss: {loss.item()}')
+            
+    def predict(self, input_data): 
+        input_data = torch.FloatTensor(input_data).to(device)
+        output = torch.sigmoid(self(input_data)).item()
+        return output > 0.5
     
