9378e021275efe663528f83911cb8bd50f6849a2
phUR99 <122969226+phUR99@users.noreply.github.com>
2025-02-20 14:42:24 +0900

    add load, save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Union
 import model
+import os
 from fastapi import FastAPI
 
 modelAnd = model.AndModel()
@@ -19,25 +20,46 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def predict(model : str, left: int, right: int):
     try:
         if model == 'And':
+            model_path = "and_model.pth"
+            if os.path.exists(model_path):
+                modelAnd.load(model_path)
+            else:
+                modelAnd.train()
+                modelAnd.save(model_path)
             result = modelAnd.predict([left, right])
         elif model == 'Xor':
+            model_path = "xor_model.pth"
+            if os.path.exists(model_path):
+                modelXor.load(model_path)
+            else:
+                modelXor.train()
+                modelXor.save(model_path)
             result = modelXor.predict([left, right])
         elif model == 'Or':
+            model_path = "or_model.pth"
+            if os.path.exists(model_path):
+                modelOr.load(model_path)
+            else:
+                modelOr.train()
+                modelOr.save(model_path)
             result = modelOr.predict([left, right])
         return {"result": result}
     except Exception as e:
-        return {"error" : f"error is occured{str(e)}"}
+        return {"error" : f"error is occurred{str(e)}"}
 
 @app.post("/train{model}")
 def train(model:str):
     try:
         if model == 'And':
-            modelAnd.train()    
+            modelAnd.train()
+            modelAnd.save("and_model.pth")
         elif model == 'Xor':
-            modelOr.train()
-        elif model == 'Or':
             modelXor.train()
+            modelXor.save("xor_model.pth")
+        elif model == 'Or':
+            modelOr.train()
+            modelOr.save("or_model.pth")
     except Exception as e:
-        return {"error" : f"error is occured{str(e)}"}    
+        return {"error" : f"error is occurred{str(e)}"}    
     return {"result" : "OK"}
-   
+
